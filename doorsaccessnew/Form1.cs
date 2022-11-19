6f8799653e088be9ced4aebbea08462acc044381
Георгий Ляпунов <lyapunovgeorgy@yandex.ru>
2022-11-19 10:05:55 +0500

    Добавьте файлы проекта.

---
+++ doorsaccessnew/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace doorsaccessnew
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        public void ManagerRole(int role)
+        {
+            switch (role)
+            {
+                //И в зависимости от того, какая роль (цифра) хранится в поле класса и передана в метод, показываются те или иные кнопки.
+                //Вы можете скрыть их и не отображать вообще, здесь они просто выключены
+                case 1:
+                    label4.Text = "Максимальный";
+                    label4.ForeColor = Color.Green;
+                    button1.Enabled = true;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    break;
+                case 2:
+                    label4.Text = "Умеренный";
+                    label4.ForeColor = Color.YellowGreen;
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    button3.Enabled = true;
+                    break;
+                case 3:
+                    label4.Text = "Минимальный";
+                    label4.ForeColor = Color.Yellow;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = true;
+                    break;
+                //Если по какой то причине в классе ничего не содержится, то всё отключается вообще
+                default:
+                    label4.Text = "Неопределённый";
+                    label4.ForeColor = Color.Red;
+                    button1.Enabled = false;
+                    button2.Enabled = false;
+                    button3.Enabled = false;
+                    break;
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            //Сокрытие текущей формы
+            this.Hide();
+            //Инициализируем и вызываем форму диалога авторизации
+            FormMain form17_Auth2 = new FormMain();
+            //Вызов формы в режиме диалога
+            form17_Auth2.ShowDialog();
+            //Если авторизации была успешна и в поле класса хранится истина, то делаем движуху:
+            if (Auth.auth)
+            {
+                //Отображаем рабочую форму
+                this.Show();
+                //Вытаскиваем из класса поля в label'ы
+                label1.Text = Auth.auth_id;
+                label2.Text = Auth.auth_fio;
+                label3.Text = "Успешна!";
+                //Красим текст в label в зелёный цвет
+                label3.ForeColor = Color.Green;
+                //Вызываем метод управления ролями
+                ManagerRole(Auth.auth_role);
+            }
+            //иначе
+            else
+            {
+                //Закрываем форму
+                this.Close();
+            }
+        }
+    }
+}
---
+++ doorsaccessnew/Form2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace doorsaccessnew
+{
+    public partial class FormAuth : Form
+    {
+        // строка подключения к БД
+        string connStr = "server=chuc.caseum.ru;port=33333;user=st_1_20_20;database=is_1_20_st20_KURS;password=65218328;";
+        //Переменная соединения
+        MySqlConnection conn;
+        //Логин и пароль к данной форме Вы сможете посмотреть в БД db_test таблице t_user
+        public FormAuth()
+        {
+            InitializeComponent();
+        }
+
+        private void FormAuth_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GetUserInfo(string login_user)
+        {
+            //Объявлем переменную для запроса в БД
+            string selected_id_stud = textBox1.Text;
+            // устанавливаем соединение с БД
+            conn.Open();
+            // запрос
+            string sql = $"SELECT * FROM t_user WHERE loginUser='{login_user}'";
+            // объект для выполнения SQL-запроса
+            MySqlCommand command = new MySqlCommand(sql, conn);
+            // объект для чтения ответа сервера
+            MySqlDataReader reader = command.ExecuteReader();
+            // читаем результат
+            while (reader.Read())
+            {
+                // элементы массива [] - это значения столбцов из запроса SELECT
+                Auth.auth_id = reader[0].ToString();
+                Auth.auth_fio = reader[1].ToString();
+                Auth.auth_role = Convert.ToInt32(reader[4].ToString());
+            }
+            reader.Close(); // закрываем reader
+            // закрываем соединение с БД
+            conn.Close();
+        }
+
+    }
+}
